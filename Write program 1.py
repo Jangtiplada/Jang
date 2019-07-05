a4aedb7b632c0c43c9c2668544383d21b7bc235b
Jangtiplada <52307178+Jangtiplada@users.noreply.github.com>
2019-07-05 14:06:48 +0700

    Add files via upload
    write program1

---
+++ Write program 1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print(10)
+
+
+# In[4]:
+
+
+print(type(10))
+
+
+# In[5]:
+
+
+print(type("Hello"))
+
+
+# In[6]:
+
+
+print(type("True"))
+
+
+# In[7]:
+
+
+print(type(10.3))
+
+
+# In[8]:
+
+
+print(type("1j"))
+
+
+# In[9]:
+
+
+print(10+3.5)
+
+
+# In[ ]:
+
+
+
+
