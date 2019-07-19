03c29238370973962ace526a90ec10c4aa73fe7c
Jangtiplada <52307178+Jangtiplada@users.noreply.github.com>
2019-07-19 16:52:29 +0700

    Add files via upload

---
+++ การเข้า password.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+usernameinput = input("user name :")
+passwordinput = input("password :")
+if usernameinput =="admin"  and passwordinput == "1234" :
+    print("Done!")
+else:
+    print("Error")
+
+
+# In[3]:
+
+
+usernameinput = input("user name :")
+passwordinput = input("password :")
+if usernameinput =="admin"  and passwordinput == "1234" :
+    print("Done")
+else:
+    print("Error")
+
+
+# In[ ]:
+
+
+
+
