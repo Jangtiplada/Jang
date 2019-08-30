aec06c6a47e894d74c74ade81f6138726be1847c
Jangtiplada <52307178+Jangtiplada@users.noreply.github.com>
2019-08-30 14:06:58 +0700

    Add files via upload

---
+++ Grade.py
@@ -0,0 +1,19 @@
+score = float(input("score (100) :"))
+if score> 100:
+    print("ERROR")
+elif score>= 80:
+    print("A")
+elif score >= 75 :
+    print("B+")
+elif score >= 70 :
+    print("B")
+elif score >= 65 :
+    print("C+")
+elif score >= 60 :
+    print("C")
+elif score >= 55 :
+    print("D+")
+elif score >= 50 :
+    print("D")
+elif score < 49 :
+    print("F")
